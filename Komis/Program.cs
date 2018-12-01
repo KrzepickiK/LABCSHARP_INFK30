576227c255b10b965b3ca2b2d9da52e123dd4a6f
Krzysztof Krzepicki <Krzysztof.Krzepicki@gmail.com>
2018-12-01 13:10:10 +0100

    cleaning....

--- Komis/Program.cs
+++ Komis/Program.cs
@@ -7,18 +7,7 @@ namespace Komis
     {
         static void Main(string[] args)
         {
-            //Pojazd panda = new Panda(5000, 10, 1999);
-            //Console.WriteLine($"Typ pojazdu: {panda.WyswietlTypPojazdu}" +
-            //    $"\nMarka: {panda.WyswietlMarke()} " +
-            //    $"\nModel: {panda.WyswietlModel()}" +
-            //    $"\nCena zakupu: {panda.CenaZakupu} zł" +
-            //    $"\nMarża: {panda.Marza}%" +
-            //    $"\nRok produkcji: {panda.RokProdukcji}" +
-            //    $"\nCena koncowa: {panda.podajCeneKoncowa()}" +
-            //    $"\nNever say no to Panda!! ;)");
-
             List<Pojazd> list = new List<Pojazd>();
-            
             string option = "m";
 
             do
@@ -29,23 +18,31 @@ namespace Komis
                         Console.WriteLine("Lista pojazdów:");
                         foreach (Pojazd poj in list)
                         {
-                            Console.WriteLine(poj.WyswietlTypPojazdu);
+                            Console.WriteLine("________________________________________");
+                            Console.WriteLine($"Typ pojazdu: {poj.WyswietlTypPojazdu}" +
+                                $"\nMarka: {poj.WyswietlMarke()} " +
+                                $"\nModel: {poj.WyswietlModel()}" +
+                                $"\nCena zakupu: {poj.CenaZakupu} zł" +
+                                $"\nMarża: {poj.Marza}%" +
+                                $"\nRok produkcji: {poj.RokProdukcji}" +
+                                $"\nCena koncowa: {poj.podajCeneKoncowa()}");
+                            if (poj.WyswietlModel()=="Panda") Console.WriteLine("___Never say no to Panda !! :)___ ");
+                            Console.WriteLine("________________________________________");
                         }
-                        option = Console.ReadLine();
+                        option = "m";
                         break;
                     case "2":
                         Console.WriteLine("Dodaj nowy pojazd.");
                         list.Add(new Panda(5000, 10, 1999));
-                        list.Add(new SuperSport(10000, 15, 2005));
-                        option = Console.ReadLine();
+                        option = "m";
                         break;
                     case "3":
                         Console.WriteLine("Wyszukaj pojazd w komisie.");
-                        option = Console.ReadLine();
+                        option = "m";
                         break;
                     case "4":
                         Console.WriteLine("Sprzedaj pojazd.");
-                        option = Console.ReadLine();
+                        option = "m";
                         break;
                     case "m":
                         Console.WriteLine("Dostępne opcje:");
@@ -53,8 +50,13 @@ namespace Komis
                         Console.WriteLine("2 : Dodaj nowy pojazd");
                         Console.WriteLine("3 : Wyszukaj pojazdy");
                         Console.WriteLine("4 : Sprzedaj pojazd");
-                        Console.WriteLine("q : wyjdź");
+                        Console.WriteLine("clear : Wyczyść ekran");
+                        Console.WriteLine("quit : Wyjdź");
                         option = Console.ReadLine();
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        option = "m";
                         break;
                     default:
                         Console.WriteLine("Brak takiej opcji.");
@@ -63,9 +65,7 @@ namespace Komis
                         option = Console.ReadLine();
                         break;
                 }
-            } while (option != "q");
-
-            //panda.WyswietlRok();
+            } while (option != "quit");
         }
     }
  
